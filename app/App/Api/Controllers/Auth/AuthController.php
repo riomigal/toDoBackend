c971db9805599f9ee6b3942b4326248c2e86c338
riomigal <saverio.migale@gmail.com>
2023-07-03 08:47:21 +0200

    Login User directly after registering

--- app/App/Api/Controllers/Auth/AuthController.php
+++ app/App/Api/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Api\Requests\LoginRequest;
 use App\Api\Requests\RegisterRequest;
 use Domain\User\Actions\LoginUserAction;
 use Domain\User\Actions\RegisterUserAction;
+use Domain\User\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Support\Http\Controllers\ApiController;
@@ -23,15 +24,7 @@ class AuthController extends ApiController
             return $this->sendError(__('Login failed! Credentials are invalid!'), null, 401);
         }
 
-        $response = [
-            'user' => $user,
-            'authorization' => [
-                'token' => $user->createToken('apiAuthToken')->plainTextToken,
-                'type' => 'bearer',
-            ]
-        ];
-
-        return $this->sendResponse(__('Login successful!'), $response);
+        return $this->sendResponse(__('Login successful!'), $this->getTokenResponse($user));
     }
 
     public function register(RegisterRequest $request, RegisterUserAction $registerUserAction): JsonResponse
@@ -40,7 +33,7 @@ class AuthController extends ApiController
 
         $user = $registerUserAction->execute($data['name'], $data['email'], $data['password']);
 
-        return $this->sendResponse(__('Account created!'), ['user' => $user], 201);
+        return $this->sendResponse(__('Account created!'), $this->getTokenResponse($user), 201);
 
     }
 
@@ -48,5 +41,16 @@ class AuthController extends ApiController
     {
         Auth::user()->tokens()->delete();
         return $this->sendResponse(__('Logout successful'), 200);
+    }
+
+    protected function getTokenResponse(User $user): array
+    {
+        return [
+            'user' => $user,
+            'authorization' => [
+                'token' => $user->createToken('apiAuthToken')->plainTextToken,
+                'type' => 'bearer',
+            ]
+        ];
     }
 }
